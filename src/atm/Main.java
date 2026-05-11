678bb24bf93a4167273f220718aeec9994492ded
Andrew Ralph <ralpaa@farmingdale.edu>
2026-05-11 13:14:51 -0400

    back end is almost completely done the account logic is working

--- src/atm/Main.java
+++ src/atm/Main.java
@@ -1,14 +1,31 @@
-public class Main(){
+import java.util.ArrayList; 
+import java.util.Scanner;
+
+public class Main{
+    public static ArrayList<AccountProfile> accounts = new ArrayList<>();
+    public static AccountProfile activeAccount = null; 
+    public static boolean isLoggedIn = false;
+
     public static void main(String[] args){
-        AccountProfile account1 = new AccountProfile(123456, 1234, 1000);
-        System.out.println("Account Number: " + account1.getAccountNumber());
-        System.out.println("Initial Balance: " + account1.getBalance());
-
-        account1.deposit(500);
-        System.out.println("Balance after deposit of $500: " + account1.getBalance());
-
-        account1.withdraw(200);
-        System.out.println("Balance after withdrawal of $200: " + account1.getBalance());
-
+        accounts.add(new AccountProfile(7456, 3239, 1000000));
+        accounts.add(new AccountProfile(2120, 0329, 7500));
+        accounts.add(new AccountProfile(5409, 6859, 2500));
+        
+        //launch JFrame
+        new ATMFrame().setVisible(true);
+    } 
+    public static boolean login(int accountNumber, String pin){
+        for(AccountProfile a : accounts){ 
+            if(a.getAccountNumber() == accountNumber && a.getPin().equals(pin)){
+                activeAccount = a;
+                isLoggedIn = true; 
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void logout(){
+        activeAccount = null;
+        isLoggedIn = false;
     }
 }
